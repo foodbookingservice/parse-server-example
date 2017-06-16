1db79f6434ee85c8944b692c12f6ca51d17a23e6
Avery Hou <avery@Averys-iMac.local>
2017-06-16 12:25:43 +0800

    add booking subsystem

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://dbuser:Dxip7758!@ds129442.mlab.com:29442/heroku_045gd6m0',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || '9tWLecLJLoApOL6sMDaP',
+  masterKey: process.env.MASTER_KEY || 'kYitPPgxatX3HIRPrD72', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://foodbooking.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -43,6 +43,10 @@ app.get('/', function(req, res) {
 // Remove this before launching your app
 app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
+app.get('/booking', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/booking'));
 });
 
 var port = process.env.PORT || 1337;
